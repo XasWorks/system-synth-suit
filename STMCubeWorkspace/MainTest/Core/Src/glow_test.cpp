82833e050fdbb113c14cb0452a470c852f222127
Xasin <davidbailey.2889@gmail.com>
2020-03-21 16:23:56 +0100

    PERFECTLY BALANCED

--- STMCubeWorkspace/MainTest/Core/Src/glow_test.cpp
+++ STMCubeWorkspace/MainTest/Core/Src/glow_test.cpp
@@ -17,16 +17,15 @@
 #include <Animation/AnimationServer.h>
 #include <Animation/NumericElement.h>
 
+#include <Animation/WaveElement.h>
+
+#include <config.h>
+
 Xasin::AnimationServer server = Xasin::AnimationServer();
 
 auto test_num_elem = Xasin::NumericElement(server, {1, 1});
 auto comp_elem = Xasin::NumericElement(server, {1, 2});
 auto num_smoother = Xasin::NumericElement(server, {1, 3});
-
-struct led_coord_t {
-	float x;
-	float y;
-};
 
 led_coord_t leds[31] = {
 		{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0},	// Stub 12 first LEDs
@@ -34,7 +33,7 @@ led_coord_t leds[31] = {
 		{-3, -1.4}, {-3, -0.7}, {-3, 0},
 		{-0.7, 4.5}, {0, 4.5}, {0.7, 4.5},
 		{2.5, 1.4}, {2.5, 0.7}, {2.5, 0},
-		{6, 1}, {7, 5}, // Thumb
+		{6, 1}, {7, 5}, // Thumb (22, 23)
 		{3, 6}, {3, 10},
 		{1.5, 7}, {1.5, 12},
 		{-1, 6}, {-1, 10},
@@ -43,64 +42,82 @@ led_coord_t leds[31] = {
 
 auto glow = Xasin::NeoController(hspi1, 31, false);
 
-float calc_position(const led_coord_t led, const led_coord_t facts) {
-	float sum = 0;
+Xasin::Layer l_gem = glow.colors;
 
-	auto led_c = reinterpret_cast<const float*>(&led);
-	auto fact_c = reinterpret_cast<const float*>(&facts);
+int flash_leds[] = {
+		21,
+		23,
+		25,
+		27,
+		29
+};
+Xasin::Color flash_colors[] = {
+		Material::GREEN,
+		Material::PURPLE,
+		Material::BLUE,
+		Material::RED,
+		Material::ORANGE,
+};
 
-	for(int i=0; i<sizeof(led_coord_t) / sizeof(float); i++) {
-		if(fact_c[i] == 0)
-			continue;
-		if(isnan(led_c[i]))
-			return 0;
+uint8_t wave_num = 0;
+void start_wave(led_coord_t center_point, Xasin::Color w_color, float speed = 20) {
+	auto n_glow = new Xasin::Animation::WaveElement(server, {2, wave_num++}, glow.colors);
 
-		sum = fmaf(led_c[i], fact_c[i], sum);
-	}
+	n_glow->wave_color = w_color;
+	n_glow->wave_color.alpha = 0.5;
 
-	return sum;
+	n_glow->x_dir = {1, 0};
+	n_glow->y_dir = {0, 1};
+	n_glow->offset = center_point;
+
+	n_glow->circle_speed = speed;
+
+	n_glow->delete_after = server.get_synch_time() + 5;
 }
+
+float last_blep_time = 3;
 
 extern "C" {
 void testGlow() {
+	Xasin::AnimationElement::led_coordinates = leds;
+
 	for(int i=0; i<12; i++)
 		leds[i] = {2*cosf(M_PI*i/6.0F), 2*sinf(M_PI*i/6.0F)};
 
-	led_coord_t spin_coords = {};
+	server.tick(0.01);
 
-	test_num_elem.type = Xasin::TIMER;
-	test_num_elem.set_flt(0, "2");
-	test_num_elem.set_flt(1, "4");
-
-	comp_elem.type = Xasin::COMP;
-	comp_elem.set_flt(0, "S1M1V2");
-	comp_elem.set_flt(1, "2");
-
-	num_smoother.type = Xasin::PT1_APPROACH;
-	num_smoother.set_flt(0, "S1M2V2");
-	num_smoother.set_flt(1, "3");
-
-	float kernel_move = 0;
-
-	server.tick(1);
-
-	num_smoother.get_flt(2)->copy_ptr = &kernel_move;
+	l_gem.fill(Xasin::Color(0, 0, 0));
+	l_gem.alpha = 0.1;
 
 	while(1) {
 		osDelay(10);
 
 		server.tick(0.01);
 
-		spin_coords = {1, 0};
+		if(server.get_synch_time() > last_blep_time) {
+			last_blep_time += 2;
+			if(wave_num < 5) {
+				int led_num = flash_leds[wave_num];
+				Xasin::Color flash_color = flash_colors[wave_num];
+				flash_color.bMod(0.8);
 
-		for(int i=0; i<12; i++) {
-			float led_pos = calc_position(leds[i], spin_coords);
+				l_gem[led_num] = flash_color;
 
-			if(fabsf(led_pos - kernel_move*4 + 2) < 0.5)
-				glow.colors[i].merge_overlay(Xasin::Color(Material::LIME, 0.7), 0.1);
-			else
-				glow.colors[i].merge_overlay(Xasin::Color(Material::BLUE, 0.25), 0.04);
+				start_wave(leds[led_num], flash_color);
+			}
+			else if(wave_num == 5) {
+				wave_num = 6;
+
+				l_gem.fill(Material::YELLOW, 0, 11);
+
+				start_wave({0, 0}, Material::YELLOW, 10);
+			}
 		}
+
+		for(int i=0; i<31; i++)
+			glow.colors[i].merge_overlay(Xasin::Color(0xE2B007, 0.3), 0.04);
+
+		glow.colors.merge_overlay(l_gem);
 
 		glow.push();
 	}
