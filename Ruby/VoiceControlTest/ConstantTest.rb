0e9ca9cff6eef48db9418f390d3707891dfc0958
Xasin <davidbailey.2889@gmail.com>
2020-05-14 14:11:24 +0200

    Use new parameter stack, forward audio, use FurComs etc.

--- Ruby/VoiceControlTest/ConstantTest.rb
+++ Ruby/VoiceControlTest/ConstantTest.rb
@@ -1,19 +1,51 @@
 
-require 'pocketsphinx-ruby'
+require '~/Xasin/system-synth-suit/tef-FurComs/Ruby/lib/tef/furcoms/SerialToMQTT.rb'
+require '~/Xasin/system-synth-suit/tef-FurComs/Ruby/lib/tef/furcoms/MQTT.rb'
 
-require '~/Xasin/system-synth-suit/tef-FurComs/Ruby/lib/tef/furcoms/Serial.rb'
 require_relative '../AnimationControl/lib/tef/Animation/Animation_Handler.rb'
+
+require_relative '../AnimationControl/lib/tef/ParameterStack/Stack.rb'
 
 require_relative '../AnimationControl/lib/tef/Sequencing/SequencePlayer.rb'
 require_relative '../AnimationControl/lib/tef/ProgramSelection/SoundCollection.rb'
 require_relative '../AnimationControl/lib/tef/ProgramSelection/SequenceCollection.rb'
 
-$port = TEF::FurComs::Serial.new('/dev/ttyACM0');
+require 'mqtt/sub_handler'
+
+$mqtt = MQTT::SubHandler.new('localhost');
+
+$direct_port = TEF::FurComs::SerialToMQTT.new('/dev/ttyACM0', $mqtt);
+$port = TEF::FurComs::MQTT.new($mqtt);
 
 $animation_core = TEF::Animation::Handler.new($port)
 
+$parameters = TEF::ParameterStack::Stack.new();
+
 $sequencePlayer = TEF::Sequencing::Player.new();
-$sequencePlayer.after_exec { $animation_core.update_tick }
+$sequencePlayer.after_exec {
+	$parameters.process_changes
+	$animation_core.update_tick
+}
+
+speech_box = $animation_core['S100M0'] = TEF::Animation::Box.new 0;
+
+speech_box.up = 3
+speech_box.left = 3;
+speech_box.right = 3;
+speech_box.down = 3;
+
+$parameters.on_recompute 'Palette/SpeechOn', 'Palette/SpeechOff', 'SpeechLevel' do
+	$parameters['SpeechColor'] = ($parameters['SpeechLevel'] ?
+		$parameters['Palette/SpeechOn'] : $parameters['Palette/SpeechOff']);
+end
+$parameters.on_change 'Palette/SpeechDelay', 'SpeechColor' do
+	speech_box.color.delay_a = $parameters['Palette/SpeechDelay']
+	speech_box.color = $parameters['SpeechColor'] || 0xFF000000
+end
+
+$parameters['Palette/SpeechOn']  = 0x0090B0
+$parameters['Palette/SpeechOff'] = 0x005050
+$parameters['Palette/SpeechDelay'] = 10
 
 $program_selector = TEF::ProgramSelection::Selector.new()
 
@@ -45,18 +77,86 @@ def play(name)
 	$sheetmap.play effect
 end
 
-play("long time")
-
-sleep 1
-
 play("power up start")
 
-$pocket_reader.recognize do |t|
-	if m = /computer ambiance level (.*)/.match(t)
-		$ambiance_level = m[1];
+$port.on_message "BTN" do |data|
+	btn_state = (data != "UP");
 
-		puts "Ambiance to level #{$ambiance_level}"
+	if btn_state
+		begin_recording()
+		puts "Recognizing"
 	else
-		play t
+		end_recording()
+		puts "Playback!"
 	end
+end
+
+$outFile = nil;
+def begin_recording()
+	$outFile = File.open("/tmp/Memeinator.raw", "wb");
+end
+
+def end_recording()
+	return unless $outFile
+
+	myFile = $outFile;
+	$outFile = nil;
+	sleep 0.01;
+
+	myFile.close();
+
+	$mqtt.publish_to 'Pocketsphinx/Request', '1';
+end
+
+$mqtt.subscribe_to 'Pocketsphinx/Result' do |result|
+	if m = /computer ambiance level (.*)/.match(result)
+		$ambiance_level = m[1];
+	else
+		play result
+	end
+end
+
+rec  = IO.popen("arecord -r 16000 -f S16_LE -R 0 --period-size 512",
+	:external_encoding=>"ASCII-8BIT");
+rec.binmode
+play = IO.popen("aplay -r 16000 -f S16_LE -R 0 --buffer-size 2048 -", "a",
+	:external_encoding=>"ASCII-8BIT");
+play.binmode
+
+read_bfr_string = "";
+current_sample_buffer = [];
+
+out_sample_buffer = [];
+echo_buffer = Array.new(6000, 0);
+
+def set_fb_frequency(freq)
+  $fb_delay = 32000 / freq;
+  $samp_increase = 40 * 2 * Math::PI / 32000;
+end
+$samp_time = 0;
+set_fb_frequency(160)
+
+loop do
+	current_sample_buffer = rec.read(512, read_bfr_string).unpack("s<*")
+
+	out_sample_buffer = [];
+
+	current_sample_buffer.each do |os|
+		$samp_time += $samp_increase;
+
+		os *= 0.01 if $outFile
+
+		os *= 0.75 + 0.25*Math.sin($samp_time);
+
+		s = os + 0.8 * (echo_buffer[-$fb_delay] + echo_buffer[-$fb_delay*2-1] + echo_buffer[-$fb_delay*2] + echo_buffer[0]*0.2)/3;
+
+		echo_buffer.shift
+		echo_buffer.push s
+
+		out_sample_buffer.push os * 0.7 + 0.3*s;
+	end
+
+	$outFile&.write current_sample_buffer.pack("s<*");
+
+	play.write out_sample_buffer.pack("s<*");
 end
